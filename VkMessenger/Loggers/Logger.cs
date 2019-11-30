341448f185d9171e04cf764c06cff42f826d5303
Max Kuzmin <max_kuzmin@outlook.com>
2019-11-30 14:35:23 +0300

    Refactoring

--- VkMessenger/Loggers/Logger.cs
+++ VkMessenger/Loggers/Logger.cs
@@ -1,55 +1,38 @@
-﻿using ru.MaxKuzmin.VkMessenger.Net;
-using System;
+﻿using System;
 using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
 using Tizen;
 
 namespace ru.MaxKuzmin.VkMessenger.Loggers
 {
+#if DEBUG
     public static class Logger
     {
         private const string Tag = "VK";
-        private const string Ip = "192.168.0.103:5100";
-        private static bool UseLogServer = false;
-
-        private static void SendToLogServer(string level, string message)
-        {
-            if (!UseLogServer) return;
-
-            Task.Run(() =>
-            {
-                try
-                {
-                    using (var client = new ProxiedWebClient())
-                        client.DownloadString($"http://{Ip}/log?level={level}&message={message}");
-                }
-                catch { }
-            });
-        }
 
         public static void Info(string text, [CallerFilePath] string file = null, [CallerMemberName] string caller = null, [CallerLineNumber]int line = 0)
         {
             Log.Info(Tag, text, file, caller, line);
-#if DEBUG
-            SendToLogServer(nameof(Info), text);
-#endif
         }
 
         public static void Debug(string text, [CallerFilePath] string file = null, [CallerMemberName] string caller = null, [CallerLineNumber]int line = 0)
         {
-#if DEBUG
-            var textWithoutLineEndings = text.Replace('\n', ' ');
-            Log.Debug(Tag, textWithoutLineEndings, file, caller, line);
-            SendToLogServer(nameof(Debug), textWithoutLineEndings);
-#endif
+            text = text.Replace('\n', ' ');
+            Log.Debug(Tag, text, file, caller, line);
         }
 
         public static void Error(Exception e, [CallerFilePath] string file = null, [CallerMemberName] string caller = null, [CallerLineNumber]int line = 0)
         {
             Log.Error(Tag, e.ToString(), file, caller, line);
-#if DEBUG
-            SendToLogServer(nameof(Error), e.ToString());
-#endif
         }
     }
+#else
+    public static class Logger
+    {
+        public static void Info(string text, string file = null, string caller = null, int line = 0) { }
+
+        public static void Debug(string text, string file = null, string caller = null, int line = 0) { }
+
+        public static void Error(Exception e, string file = null, string caller = null, int line = 0) { }
+    }
+#endif
 }
